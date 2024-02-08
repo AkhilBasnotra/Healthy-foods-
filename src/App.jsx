27a0eddad0fa7590c2426beaced19186fd34f5cc
Akhil Basnotra <akhilbasnotra24@gmail.com>
2024-02-08 23:31:26 +0530

    added app basic layout

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <>
+      <h1>Healthy Foods</h1>;
+      <ul>
+        <li>Dark Green Vegetables</li>
+        <li>Whole Grains</li>
+        <li>Oatmeal</li>
+      </ul>
+    </>
+  );
+}
+
+export default App;
